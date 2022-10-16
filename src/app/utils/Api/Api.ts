7861a3033aa1c238d02f3df1ef8027986d8460fa
weber costa <86451517+webersilv4@users.noreply.github.com>
2022-10-16 16:02:54 -0300

    Update Api.ts

--- src/app/utils/Api/Api.ts
+++ src/app/utils/Api/Api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.BASE_URL_API
+    baseURL: import.meta.env.VITE_BASE_URL_API
 });
 
 // https://to-do-weber.herokuapp.com
